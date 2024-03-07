724dcbef7d5d908ac84f6397704d37f76be852aa
CuDuyDuc <duccu1403@gmail.com>
2024-03-08 00:38:06 +0700

    Add New

---
+++ src/assets/images/Images.ts
@@ -0,0 +1,23 @@
+const IMAGES = {
+    Splash_Logo: require('./splash_screen.png'),
+    Onboarding: require('./onboarding.png'),
+    Onboarding1: require('./onboarding1.png'),
+    Onboarding2: require('./onboarding2.png'),
+    LogoLogin : require('./login.png'),
+    Home : require('./home.png'),
+    Cart : require('./cart.png'),
+    Favorites : require('./favorites.png'),
+    OderHistory : require('./order_history.png'),
+    Setting: require('./setting.png'),
+    Avata: require('./avata.png'),
+    DolaMoney: require('./dolaMoney.png'),
+    TagCoffee: require('./iconTagCoffee.png'),
+    Back_Icon: require('./back_icon.png'),
+    Profile: require('./profile.png'),
+    heart: require('./heart.png'),
+    Coffee: require('./coffee.png'),
+    Milk: require('./milk.png'),
+    Cappuccino: require('./cappuccino_pic_1_portrait.png'),
+}
+
+export default IMAGES;
